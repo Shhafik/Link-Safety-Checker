81ce93a3db04e6c8c09d790b428ffdf29eaf3e59
Shafi <shafi36925801@gmail.com>
2025-04-20 21:29:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,6 @@ async function checkURL() {
     const resultDiv = document.getElementById('result');
     resultDiv.textContent = 'Checking...';
 
-    const cors = require('cors');
-const express = require('express');
-const app = express();
-
-// 👇 CORS config here
-const allowedOrigins = ['https://link-backend-ethn.vercel.app'];
-
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'OPTIONS'],
-  credentials: true,
-}));
-
-  
     try {
       const response = await fetch('https://link-backend-murex.vercel.app/check', {
         method: 'POST',
